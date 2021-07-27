ca2e504930a0c2c4875db5d4f36d452388ea56ab
DylanvanBallegooijen <78920682+DylanvanBallegooijen@users.noreply.github.com>
2021-07-27 10:11:37 +0200

    Add files via upload

---
+++ Opdracht Opdracht- Iterators - oude en nieuwe stijl/colors.js
@@ -0,0 +1,16 @@
+/*let selectedColours = ['yellow', 'blue', 'red', 'orange'];
+console.log(selectedColours);*/
+
+/*const array1 = ['yellow', 'blue', 'red', 'orange'];
+
+array1.forEach(element => console.log(element));
+
+const colours = ['yellow', 'blue', 'red', 'orange'];*/
+    
+
+const colors = [`'yellow', 'blue', 'red', 'orange'`];
+    colors.forEach(element => console.log(element));
+  
+
+   
+
